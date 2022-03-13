086677a0caf4e1f1b0cbb31063d65b112b8cd501
Nguyen Binh <nguyenvulebinh@gmail.com>
2022-03-13 21:45:22 +0100

    Update mrc_model.py

--- model/mrc_model.py
+++ model/mrc_model.py
@@ -22,13 +22,6 @@ class MRCQuestionAnswering(RobertaPreTrainedModel):
 
         self.init_weights()
 
-    @add_start_docstrings_to_model_forward(ROBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
-    @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
-        checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=QuestionAnsweringModelOutput,
-        config_class=_CONFIG_FOR_DOC,
-    )
     def forward(
             self,
             input_ids=None,
